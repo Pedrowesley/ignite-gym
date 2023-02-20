96edb1f6a2fa6cfc61d991fe91dc6dd1f265e9e5
Pedrowesley <pedrodev101@gmail.com>
2023-02-20 12:51:00 -0300

    create context navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { THEME } from './src/theme';
+import { THEME } from "./src/theme";
 
-import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}{" "}
     </NativeBaseProvider>
   );
 }
