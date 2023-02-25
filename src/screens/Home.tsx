efba710a154fd9e9e13f12f2b85665574ba56a8c
Pedrowesley <pedrodev101@gmail.com>
2023-02-24 23:55:59 -0300

    Create component ExerciseCard

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
-import { FlatList, HStack, VStack } from "native-base";
+import { useState } from "react";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { useState } from "react";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState("Costas");
@@ -35,6 +36,21 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
     </VStack>
   );
 }
